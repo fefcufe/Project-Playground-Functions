b95a033abb4b2138b2d87c4499348fbeaf29a519
Fernanda Ferreira <fefcufe@gmail.com>
2021-12-02 19:45:18 -0300

    desafios 7 e 8 feitos

--- src/challenges.js
+++ src/challenges.js
@@ -79,19 +79,59 @@ function highestCount(array) {
   return count;
 }
 
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
-console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
-console.log(highestCount([0, 0, 0]));
+// console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+// console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+// console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let cat2wins = 'cat2';
+  let cat1wins = 'cat1';
+  let bothLose = 'os gatos trombam e o rato foge';
+
+  let deltaScat1 = mouse - cat1;
+  if (deltaScat1 < 0) {
+    deltaScat1 = deltaScat1 * (-1);
+  }
+  let deltaScat2 = mouse - cat2;
+  if (deltaScat2 < 0) {
+    deltaScat2 = deltaScat2 * (-1);
+  }
+
+  if (deltaScat1 > deltaScat2) {
+    return cat2wins;
+  } else if (deltaScat2 > deltaScat1) {
+    return cat1wins;
+  } else {
+    return bothLose;
+  }
 }
 
+// console.log(catAndMouse(5,2,3));
+// console.log(catAndMouse(12,6,0));
+// console.log(catAndMouse(10,0,0));
+
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  finalString =[];
+
+  for (let number of array) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      finalString.push('fizzBuzz');
+    } else if (number % 3 === 0) {
+      finalString.push('fizz');
+    } else if (number % 5 === 0) {
+      finalString.push('buzz')
+    } else {
+      finalString.push('bug!');
+    }
+  }
+  return finalString;
 }
+
+// console.log(fizzBuzz([2, 15, 7, 9, 45]));
+// console.log(fizzBuzz([7, 9]));
+// console.log(fizzBuzz([9, 25]));
 
 // Desafio 9
 function encode() {
