fd0e6a21e51781f3a930de14a3837e0236fe292a
Fernanda Ferreira <fefcufe@gmail.com>
2021-12-02 18:08:17 -0300

    desafio 5 feito

--- src/challenges.js
+++ src/challenges.js
@@ -42,17 +42,22 @@ function splitSentence(string) {
 function concatName(array) {
   let lastPosition = array.length - 1;
   let result = array[lastPosition] + ', ' + array[0];
-  console.log(result);
+  return result;
 }
 
-// concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
-// concatName(['foguete', 'não', 'tem', 'ré']);
-// concatName(['captain', 'my', 'captain']);
+// console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+// console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+// console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = wins * 3 + ties;
+  return (points);
 }
+
+ // console.log(footballPoints(14,8));
+ // console.log(footballPoints(1,2));
+ // console.log(footballPoints(0,0));
 
 // Desafio 6
 function highestCount() {
